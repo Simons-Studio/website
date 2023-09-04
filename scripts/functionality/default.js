ab582b65874bfac7ea93d06b740b66d2b8aaf1a9
Simon Bennett <sim.j.bennett@gmail.com>
2023-09-04 23:06:34 +1000

    close nav menu on header swap

--- scripts/functionality/default.js
+++ scripts/functionality/default.js
@@ -2,23 +2,32 @@ let header = document.getElementById("header");
 let headerHeight = header.clientHeight;
 window.onscroll = () => positionHeader();
 
+let navigationButton = document.getElementById("navigation-menu-input");
+let expandableNavMenu = document.getElementById("expansion-menu");
+
+// TODO: Smooth out menu transition
 function positionHeader() {
   let distBottomHeadertoScreenTop = window.scrollY - headerHeight;
   if (distBottomHeadertoScreenTop > 0 && screen.width < 600) {
-    header.classList.add("nav-bar-attach-bottom");
+    if (!header.classList.contains("nav-bar-attach-bottom")) {
+      header.classList.add("nav-bar-attach-bottom");
+      navigationButton.checked = false;
+      toggleDivExpansion(expandableNavMenu, false);
+    }
     let headerPeak = min(distBottomHeadertoScreenTop - headerHeight, 0);
     header.style.bottom = headerPeak + "px";
   } else {
-    header.classList.remove("nav-bar-attach-bottom");
+    if (header.classList.contains("nav-bar-attach-bottom")) {
+      header.classList.remove("nav-bar-attach-bottom");
+      navigationButton.checked = false;
+      toggleDivExpansion(expandableNavMenu, false);
+    }
   }
 }
 
-document
-  .getElementById("navigation-menu-input")
-  .addEventListener("change", function (e) {
-    let expandableNavMenu = document.getElementById("expansion-menu");
-    toggleDivExpansion(expandableNavMenu, this.checked);
-  });
+navigationButton.addEventListener("change", function (e) {
+  toggleDivExpansion(expandableNavMenu, this.checked);
+});
 
 function toggleDivExpansion(div, checked) {
   let contentHeight = div.scrollHeight;
