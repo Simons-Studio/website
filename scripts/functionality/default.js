0a9f4e2bc2951fdd18d3d4bbec0050970c4de6e3
Simon Bennett <sim.j.bennett@gmail.com>
2023-09-04 15:50:37 +1000

    vertical collapsable navbar

--- scripts/functionality/default.js
+++ scripts/functionality/default.js
@@ -1,17 +1,36 @@
+let topBar = document.getElementById("topbar");
+// let sticky = topBar.offsetTop;
 
-let topBar = document.getElementById('topbar');
-let sticky = topBar.offsetTop;
+// function positionHeader() {
+//   if (window.scrollY > sticky + topBar.clientHeight) {
+//     console.log(sticky);
+//     topBar.classList.add("nav-bar-attach-bottom");
+//     // topBar.classList.remove('nav-bar-attach-top');
+//   } else {
+//     topBar.classList.remove("nav-bar-attach-bottom");
+//     // topBar.classList.add('nav-bar-attach-top');
+//   }
+// }
 
-function positionHeader() {
-    
-    if (window.scrollY > sticky + topBar.clientHeight) {
-        console.log(sticky);
-        topBar.classList.add('nav-bar-attach-bottom');
-        // topBar.classList.remove('nav-bar-attach-top');
-    } else {
-        topBar.classList.remove('nav-bar-attach-bottom');
-        // topBar.classList.add('nav-bar-attach-top');
-    }
+// window.onscroll = function () {
+//   positionHeader();
+// };
+
+document
+  .getElementById("navigation-menu-button")
+  .addEventListener("change", function (e) {
+    let expandableNavMenu = document.getElementById("expansion-menu");
+    toggleDivExpansion(expandableNavMenu, this.checked);
+  });
+
+function toggleDivExpansion(div, checked) {
+  let contentHeight = div.scrollHeight;
+
+  if (checked) {
+    div.style.visibility = "visible";
+    div.style.maxHeight = contentHeight + "px";
+  } else {
+    div.style.visibility = "hidden";
+    div.style.maxHeight = 0 + "px";
+  }
 }
-
-window.onscroll = function() {positionHeader();}
