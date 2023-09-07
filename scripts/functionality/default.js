ed2f8b54eac6296d745a4eb30b622d03a6829d64
Simon Bennett <sim.j.bennett@gmail.com>
2023-09-07 10:53:18 +1000

    repositioning header only occurs on small screen

--- scripts/functionality/default.js
+++ scripts/functionality/default.js
@@ -7,24 +7,26 @@ let expandableNavMenu = document.getElementById("expansion-menu");
 
 // TODO: Smooth out menu transition
 function positionHeader() {
-  let distBottomHeadertoScreenTop = window.scrollY - headerHeight;
-  if (distBottomHeadertoScreenTop > 0 && screen.width < 600) {
-    if (!header.classList.contains("nav-bar-attach-bottom")) {
-      header.classList.add("nav-bar-attach-bottom");
-      navigationButton.checked = false;
-      expandableNavMenu.style.display = "none";
-      toggleDivExpansion(expandableNavMenu, false);
-      expandableNavMenu.style.display = "flex";
-    }
-    let headerPeak = min(distBottomHeadertoScreenTop - headerHeight, 0);
-    header.style.bottom = headerPeak + "px";
-  } else {
-    if (header.classList.contains("nav-bar-attach-bottom")) {
-      header.classList.remove("nav-bar-attach-bottom");
-      navigationButton.checked = false;
-      expandableNavMenu.style.display = "none";
-      toggleDivExpansion(expandableNavMenu, false);
-      expandableNavMenu.style.display = "flex";
+  if (screen.width < 600) {
+    let distBottomHeadertoScreenTop = window.scrollY - headerHeight;
+    if (distBottomHeadertoScreenTop > 0) {
+      if (!header.classList.contains("nav-bar-attach-bottom")) {
+        header.classList.add("nav-bar-attach-bottom");
+        navigationButton.checked = false;
+        expandableNavMenu.style.display = "none";
+        toggleDivExpansion(expandableNavMenu, false);
+        expandableNavMenu.style.display = "flex";
+      }
+      let headerPeak = min(distBottomHeadertoScreenTop - headerHeight, 0);
+      header.style.bottom = headerPeak + "px";
+    } else {
+      if (header.classList.contains("nav-bar-attach-bottom")) {
+        header.classList.remove("nav-bar-attach-bottom");
+        navigationButton.checked = false;
+        expandableNavMenu.style.display = "none";
+        toggleDivExpansion(expandableNavMenu, false);
+        expandableNavMenu.style.display = "flex";
+      }
     }
   }
 }
