71f1e2277eb4a0a6696e351a148e1ae1801e7f17
Simon <sim.j.bennett@gmail.com>
2026-03-10 20:36:00 +1100

    add initial PI concept files

---
+++ pi/pi.js
@@ -0,0 +1,17 @@
+let background_digits = document.getElementById("background-digits")
+
+let x = fetch("1-10000.txt")
+
+background_digits.textContent = ""
+
+function pi_digits(n, base = 10) {
+
+}
+
+function chudnovsky_algorithm(max_n) {
+
+}
+
+function step(k) {
+  Decimal()
+}
